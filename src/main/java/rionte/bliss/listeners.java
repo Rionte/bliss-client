e94e9083a755d39622dd11811a0acdce2cfe6e41
Teddsy2 <107900060+Teddsy2@users.noreply.github.com>
2024-04-04 22:28:19 -0400

    use dot to open chat

--- src/main/java/rionte/bliss/listeners.java
+++ src/main/java/rionte/bliss/listeners.java
@@ -6,8 +6,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiChat;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -18,6 +21,7 @@ import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import rionte.bliss.commands.AnalyzerCommand;
@@ -134,6 +138,13 @@ public class listeners {
 		} */
 		
 		totalTicks += 1;
+	}
+	
+	@SubscribeEvent
+	public void onKeyPress(InputEvent.KeyInputEvent event) {
+	    if(Keyboard.isKeyDown(Keyboard.KEY_PERIOD)) {
+	    	mc.displayGuiScreen(new GuiChat("."));
+	    }
 	}
 	
 	@SubscribeEvent
